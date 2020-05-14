421e9009062762f9a5961896f079885780cd43a2
wrunion <winterrunion@gmail.com>
2020-05-14 00:28:40 -0700

    LetterControl now passes props to HangmanImage succesfully.

--- src/components/HangmanImage.js
+++ src/components/HangmanImage.js
@@ -3,8 +3,9 @@ import image3 from '../assets/hangman3.png';
 import './HangmanImage.css';
 
 function HangmanImage(props) {
-  const { number } = props.number;
+  const { number } = props;
   // const imageToShow = `image${number}`;
+  console.log(number);
   return (
     <div className="HangmanImage">
       {/* <img src={`image${number}`} alt="" className="HangmanImage" />   */}
