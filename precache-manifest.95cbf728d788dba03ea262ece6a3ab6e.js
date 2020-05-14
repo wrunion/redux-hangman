0a8e5ccb31a5939b245a935ef8ba50c4b34105ab
wrunion <winterrunion@gmail.com>
2020-05-14 12:28:57 -0700

    Updates

---
+++ precache-manifest.95cbf728d788dba03ea262ece6a3ab6e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f955f762e298388c0d634a91e9c58746",
+    "url": "/redux-hangman/index.html"
+  },
+  {
+    "revision": "67f008c5c3b83c86573f",
+    "url": "/redux-hangman/static/css/main.e1008442.chunk.css"
+  },
+  {
+    "revision": "00c68669b815789aed97",
+    "url": "/redux-hangman/static/js/2.d90383da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-hangman/static/js/2.d90383da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f008c5c3b83c86573f",
+    "url": "/redux-hangman/static/js/main.d084b354.chunk.js"
+  },
+  {
+    "revision": "f781b267013ed950db7d",
+    "url": "/redux-hangman/static/js/runtime-main.6fd8fe6f.js"
+  },
+  {
+    "revision": "bbde16d0fa9b24e7c80692c531a0f272",
+    "url": "/redux-hangman/static/media/image0.bbde16d0.png"
+  },
+  {
+    "revision": "1b639a7308191db9e66d15f158659188",
+    "url": "/redux-hangman/static/media/image1.1b639a73.png"
+  },
+  {
+    "revision": "d9b64e23c50383fdfb6d9a7a0f2f9f38",
+    "url": "/redux-hangman/static/media/image2.d9b64e23.png"
+  },
+  {
+    "revision": "af97f4201553e29dbb72766b83c1b3cd",
+    "url": "/redux-hangman/static/media/image3.af97f420.png"
+  },
+  {
+    "revision": "d30ae963a6ecf47470e4b814c38afd79",
+    "url": "/redux-hangman/static/media/image4.d30ae963.png"
+  },
+  {
+    "revision": "1fac6abaa79450f2bc4e26f57edd355c",
+    "url": "/redux-hangman/static/media/image5.1fac6aba.png"
+  },
+  {
+    "revision": "0e6181ddd26279f6f4ec2f2bfa95a83b",
+    "url": "/redux-hangman/static/media/image6.0e6181dd.png"
+  },
+  {
+    "revision": "bdcfcb3aacec40206a7c4478ba0c089f",
+    "url": "/redux-hangman/static/media/image7.bdcfcb3a.png"
+  }
+]);
