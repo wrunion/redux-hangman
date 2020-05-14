763d62b6498b8719e255f44d7b64aa019f57f2e7
wrunion <winterrunion@gmail.com>
2020-05-14 12:37:04 -0700

    Add heading to LetterDisplay.

--- src/components/LetterDisplay.js
+++ src/components/LetterDisplay.js
@@ -3,6 +3,7 @@ import React from 'react'
 function LetterDisplay(props) {
   return (
     <div className="LetterDisplay">
+      <h4>You have guessed:</h4>
       {props.letterArray.join('')}
     </div>
   )
