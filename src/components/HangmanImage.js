055dbf8ed8986fc10af29b19de8334eebe18a1f6
wrunion <winterrunion@gmail.com>
2020-05-14 00:36:07 -0700

    Commit before bed.

--- src/components/HangmanImage.js
+++ src/components/HangmanImage.js
@@ -4,14 +4,16 @@ import './HangmanImage.css';
 
 function HangmanImage(props) {
   const { number } = props;
-  // const imageToShow = `image${number}`;
-  console.log(number);
+  if (number === "3") {
   return (
     <div className="HangmanImage">
       {/* <img src={`image${number}`} alt="" className="HangmanImage" />   */}
       <img src={image3} alt="" />
     </div>
-  )
+  );
+  } else {
+    return <div>[Image will go here. Number is: {number}]</div>
+  }
 }
 
 export default HangmanImage;
